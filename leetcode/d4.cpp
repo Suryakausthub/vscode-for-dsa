7323c103504366fdfbf990d1d2c7414308625875
Suryakausthub <kausthubsurya@gmail.com>
2024-06-20 19:10:25 +0530

    700. Search in a Binary Search Tree

---
+++ leetcode/d4.cpp
@@ -0,0 +1,31 @@
+/**700. Search in a Binary Search Tree
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (root == nullptr || root->val == val) {
+            return root;
+        }
+        if (root->val < val) {
+            return searchBST(root->right, val);
+        } else {
+            return searchBST(root->left, val);
+        }
+    }
+};
+
+void preorder(TreeNode* root) {
+    if (root == nullptr) return;
+    cout << root->val << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
